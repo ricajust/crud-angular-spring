a5b88c034db008d4339a4b6784df4629d88ec63e
ricajust <ricartoonstudio@gmail.com>
2026-01-20 21:59:17 -0300

    feat: add courses component and angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Courses} from './courses/courses/courses';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Courses],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/courses/courses.routes.ts
@@ -0,0 +1,7 @@
+// courses/courses.routes.ts
+import { Routes } from '@angular/router';
+import {Courses} from './courses/courses';
+
+export const COURSES_ROUTES: Routes = [
+  { path: '', component: Courses }
+];
